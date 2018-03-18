d3b772b89e0a53466dc98e9899539fe91a2d56ea
Patrick Ly <patrickly@users.noreply.github.com>
2018-03-18 15:31:54 -0700

    Client and Server Set up (#3)
    * added line 5 to readme
    
    * line 7 added
    
    * client and server code set up

---
+++ server/config.js
@@ -0,0 +1,14 @@
+/*
+module.exports = {
+  database: 'mongodb:/csulbDevs:491@ds117539.mlab.com:17539/wastenotwebapp',
+  port: 3030,
+  secret: "CsulbDevs123456789"
+};
+*/
+
+module.exports = {
+  database:
+  'mongodb://csulbDevs:491@ds117539.mlab.com:17539/wastenotwebapp',
+  port: 3030,
+  secret: 'CsulbDevs2012312321'
+};
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./config');
+
+const app = express();
+
+mongoose.connect(config.database, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res, next) =>{
+  res.json({
+    user: 'Pat Ly'
+  });
+});
+
+
+
+app.listen(config.port, (err) => {
+  console.log("Server started on port " + config.port);
+});
