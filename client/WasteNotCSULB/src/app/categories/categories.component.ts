39f126b59567a1bfb8e8f28add68b91319937295
patrickly <patrickkimly@gmail.com>
2018-04-30 15:43:40 -0700

    Categories Component and Html

--- client/WasteNotCSULB/src/app/categories/categories.component.ts
+++ client/WasteNotCSULB/src/app/categories/categories.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RestApiService } from '../rest-api.service';
+import { DataService } from '../data.service';
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  categories: any;
 
-  constructor() { }
+  constructor(
+    private data: DataService,
+    private rest: RestApiService
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      const data = await this.rest.get(
+        'http://localhost:3030/api/categories'
+      );
+      data['success']
+        ? (this.categories = data['categories'])
+        : this.data.error(data['message']);
+    } catch (error) {
+      this.data.error(error['message']);
+    }
   }
 
 }
