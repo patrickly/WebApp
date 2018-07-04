6fe9a5b8b33143bba5f8e5cc5ae6d8512fa080c0
Patrick Ly <patrickly@users.noreply.github.com>
2018-07-03 22:20:23 -0700

    7.3.18 pat bootstrap responsive quiz (#27)
    * adding responsiveness to quiz
    
    * test commit
    
    * removing characters
    
    * quiz on mobile looks better than quiz on desktop
    
    * quiz looks nicer on both mobile and desktop
    
    * added border for each answer choice. removed purple padding from image
    
    * aligned question and question number
    
    * Question 1 is mobile friendly and responsive on both desktop and smartphones
    
    * Full placeholder sentence can now be seen in the search bar
    
    * questions 1 to 3 for quiz. changed alt for images
    
    * All 10 questions for quiz
    
    * Display the correct choice for all questions after answering

--- client/WasteNotCSULB/src/app/quiz/quiz.component.ts
+++ client/WasteNotCSULB/src/app/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { RestApiService } from '../rest-api.service';
@@ -10,8 +10,7 @@ import { DataService } from '../data.service';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
-
-  idx : number = 0;
+  idx: number = 0;
 
   btnDisabled = true;
 
@@ -19,7 +18,7 @@ export class QuizComponent implements OnInit {
 
   obj4: any = {
     answered: false
-  }
+  };
 
   questionResultRight: any = null;
   questionResultWrong: any = null;
@@ -28,128 +27,107 @@ export class QuizComponent implements OnInit {
 
   quizFinished: boolean = false;
 
-  constructor( 
+  constructor(
     private data: DataService,
     private rest: RestApiService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
-    async ngOnInit() {
-      try {
-        const data = await this.rest.get(
-          'http://localhost:3030/api/itemsRandom'
-        );
-        data['success']
-          ? (this.items = data['items'])
-          : this.data.error(data['message']);
+  async ngOnInit() {
+    try {
+      const data = await this.rest.get('http://localhost:3030/api/itemsRandom');
+      data['success']
+        ? (this.items = data['items'])
+        : this.data.error(data['message']);
 
-        if(data['success']){
-     
-          this.items.forEach(element => {
-           element= Object.assign(element, this.obj4 );
+      if (data['success']) {
+        this.items.forEach(element => {
+          element = Object.assign(element, this.obj4);
 
-           //console.log(JSON.stringify(element));
-
-          });
-         this.shuffle(this.items);
-
-        } else {
-          console.log("TTTT error"); 
-        }
-      } catch (error) {
-        this.data.error(error['message']);
+          //console.log(JSON.stringify(element));
+        });
+        this.shuffle(this.items);
+      } else {
+        console.log('TTTT error');
       }
+    } catch (error) {
+      this.data.error(error['message']);
     }
-
-    myFunction(){
-      
-      if (this.idx >= 9) {
-        this.quizFinished = true;
-      } 
-      this.btnDisabled = true;
-      this.questionResultRight = null;
-      this.questionResultWrong = null;
-      this.idx += 1;
-    }
-
-    submitAnswer(itemIndex: number, choiceNum: number ){
-
-      var tempChoice: string = '';
-      
-  
-
-       if(choiceNum === 7){
-        tempChoice = 'compost';
-       }
-       if(choiceNum === 8){
-        tempChoice = 'recycle';
-
-       }
-       if(choiceNum === 9){
-        tempChoice = 'landfill';
-       }
-
-      
-
-       if(tempChoice.includes(this.items[itemIndex].category.name)){
-            this.questionResultRight = "Correct";
-            this.totalRight += 1;
-       } else {
-            this.questionResultWrong = "Incorrect";
-       }
-
-       this.items[itemIndex].answered = true;
-       if( this.items[itemIndex].answered ) {
-         this.btnDisabled = false;
-       }
-
-       if (this.totalRight > 0){
-        this.scorePercent = (this.totalRight/(itemIndex + 1) * 100); 
-       } else {
-        this.scorePercent = 0;
-       }
-      
-       if(itemIndex > 9){
-        this.quizFinished = true;
-       }
-  
-    } //submitAnswer
-
-
-    shuffle(array) : void {
-
-      let counter = array.length;
-  
-      // While there are elements in the array
-      while (counter > 0) {
-          // Pick a random index
-          let index = Math.floor(Math.random() * counter);
-  
-          // Decrease counter by 1
-          counter--;
-  
-          // And swap the last element with it
-          let temp = array[counter];
-          array[counter] = array[index];
-          array[index] = temp;
-      }
-  
-  } // shuffle
-
-  exitQuiz() : void {
-    this.router.navigate(['/']);
-
   }
 
-  refresh() : void {
+  myFunction() {
+    if (this.idx >= 9) {
+      this.quizFinished = true;
+    }
+    this.btnDisabled = true;
+    this.questionResultRight = null;
+    this.questionResultWrong = null;
+    this.idx += 1;
+  }
 
+  submitAnswer(itemIndex: number, choiceNum: number) {
+    var tempChoice: string = '';
+
+    if (choiceNum === 7) {
+      tempChoice = 'compost';
+    }
+    if (choiceNum === 8) {
+      tempChoice = 'recycle';
+    }
+    if (choiceNum === 9) {
+      tempChoice = 'landfill';
+    }
+
+    if (tempChoice.includes(this.items[itemIndex].category.name)) {
+      this.questionResultRight = 'Correct';
+      this.totalRight += 1;
+    } else {
+      this.questionResultWrong = 'Incorrect';
+    }
+
+    this.items[itemIndex].answered = true;
+    if (this.items[itemIndex].answered) {
+      this.btnDisabled = false;
+    }
+
+    if (this.totalRight > 0) {
+      this.scorePercent = (this.totalRight / (itemIndex + 1)) * 100;
+    } else {
+      this.scorePercent = 0;
+    }
+
+    if (itemIndex > 9) {
+      this.quizFinished = true;
+    }
+  } //submitAnswer
+
+  shuffle(array): void {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+  } // shuffle
+
+  exitQuiz(): void {
+    this.router.navigate(['/']);
+  }
+
+  refresh(): void {
     window.location.reload();
   }
 
-  update() : void {
+  update(): void {
     this.ngOnInit();
-} 
-
-
+  }
 }
-
-
