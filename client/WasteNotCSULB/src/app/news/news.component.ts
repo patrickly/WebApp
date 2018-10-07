8e069be50b174d5d5f31e2382eb13c45f4efb011
Greg Paolo Violan <violan.greg@gmail.com>
2018-10-06 19:36:35 -0700

    Front-end update (#35)
    * home-page refactoring
    
    refactored home-page code with bootstrap 4, added compost, recycle, landfill quick-access.
    
    * Clicking on a category image on the home page now takes a user to the list of items based on that category.
    
    * Auto stash before merge of "front-end" and "origin/front-end"
    
    * Auto stash before merge of "front-end" and "origin/front-end"
    
    * front-end changes: navbar and added more quickaccess
    
    added categorize, browse, news, map, upload item photo, about, and changed navbar fixed to bottom
    
    * Top banner
    
    Top banner html & css
    
    * changed compost, recycle, landfill icon and icon size
    
    * Fixed footer functionality
    
    - Functions as absolute footer on mobile
    - On desktop, attaches to bottom of content to remove whitespace between footer and page content
    
    * Re-added angular-cli.json file.
    
    * Fix closing div and nav tag issues.
    
    * Top navbar edits
    
    - Moved waste not line to top right, disappears with low width mobile devices.
    
    * nav-bar rework, added search bar on items (browse) component
    
    * Logo changes & About page & navbar color
    
    -Logo changes done, resized, and created new one
    -About page created, matches Github page
    -Changed navbar colors
    
    * Refactored item info page
    
    * Navbar fix
    
    items on navbar are now evenly vertically centered
    
    * New Waste Not Assets
    
    - Added fonts, implemented into styles.scss
    - Added new logo, white
    
    * Added angular.json, changed home, nav-bar, news, about components
    
    Used assets for compost, recycle, and landfill. Changed news icon to quiz icon on nav-bar, Created the template for news.component.ts, Added the news list on the html. Added support section on about page.
    
    * added algolia logo
    
    * fixed search bar to append, fixed item component logos
    
    * added news link to csulb/sustainability/news

--- client/WasteNotCSULB/src/app/news/news.component.ts
+++ client/WasteNotCSULB/src/app/news/news.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RestApiService } from '../rest-api.service';
+import { DataService } from '../data.service';
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
+  news: any;
 
-  constructor() { }
+  newNews = '';
+  btnDisabled = false;
 
-  ngOnInit() {
+  constructor(
+    private data: DataService,
+    private rest: RestApiService
+  ) { }
+
+  async ngOnInit() {
+    try {
+      const data = await this.rest.get(
+        'http://localhost:3030/api/news'
+      );
+      data['success']
+      ? (this.news = data['news'])
+      : this.data.error(data['message']);
+    } catch (error) {
+      this.data.error(error['message']);
+    }
+  }
+
+  async addNews() {
+    this.btnDisabled = true;
+    try {
+      const data = await this.rest.post(
+        'http://localhost:3030/api/news',
+        { _news: this.newNews }
+      );
+      data['success']
+        ? this.data.success(data['message'])
+        : this.data.error(data['message']);
+    } catch (error) {
+      this.data.error(error['message']);
+    }
+    this.btnDisabled = false;
   }
 
 }
