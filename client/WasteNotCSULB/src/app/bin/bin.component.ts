024c78b3bd4052004538d1ba37a8d871c1bfa944
Greg Paolo Violan <violan.greg@gmail.com>
2018-10-22 17:29:28 -0700

    Added case statements for bin descriptions, changed page count of items

--- client/WasteNotCSULB/src/app/bin/bin.component.ts
+++ client/WasteNotCSULB/src/app/bin/bin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RestApiService } from '../rest-api.service';
 import { DataService } from '../data.service';
 import { environment } from "../../environments/environment";
@@ -14,27 +14,30 @@ const BACKEND_URL = environment.api;
 export class BinComponent implements OnInit {
   binId: any;
   bin: any;
+  binType: any;
   page = 1;
 
   constructor(
     private data: DataService,
     private activatedRoute: ActivatedRoute,
     private rest: RestApiService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(res => {
       this.binId = res['id'];
       this.getItems();
+      this.getBinType();
     });
   }
 
   get lower() {
-    return 10 * (this.page - 1) + 1;
+    return 25 * (this.page - 1) + 1;
   }
 
   get upper() {
-    return Math.min(10 * this.page, this.bin.totalItems);
+    return Math.min(25 * this.page, this.bin.totalItems);
   }
 
   async getItems(event?: any) {
@@ -51,6 +54,16 @@ export class BinComponent implements OnInit {
         : this.data.error(data['message']);
     } catch (error) {
       this.data.error(error['message']);
+    }
+  }
+
+  getBinType() {
+    if (this.binId === '5ace82a94561ae0ecf27a16a') {
+      this.binType = 1;
+    } else if (this.binId === '5ae79d9d027d2834db315428') {
+      this.binType = 2;
+    } else if (this.binId === '5ae79d8e027d2834db315427') {
+      this.binType = 3;
     }
   }
 
